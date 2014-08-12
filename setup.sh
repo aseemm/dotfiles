9020e8fe1c3953ebdd1d9039a37b56d4e6eafc74
Aseem Maheshwari <aseemm@gmail.com>
2014-08-12 11:07:48 -0700

    install gtkwave as well

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ sudo apt-get install aterm
 # g++, etc
 sudo apt-get install build-essential
 sudo apt-get install ddd
+sudo apt-get install gtkwave
 
 # chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
