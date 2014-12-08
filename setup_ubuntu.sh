9053e77f1d4fd8fbfb2874059e8017ccdb8e3b85
Aseem Maheshwari <aseemm@gmail.com>
2014-12-08 14:50:51 -0800

    remove flask details

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -22,33 +22,8 @@ sudo apt-get install google-chrome-stable
 ### google-chrome
 
 # python3
-sudo apt-get install curl
-sudo apt-get install python3
-
-# flash (within venv)
-  #  95  python3 -m venv flask --without-pip
-  #  97  source flask/bin/activate
-  # 100  cd flask
-  # 101  mkdir pypioffline
-  # 102  cd pypioffline/
-  # 105  curl -O https://pypi.python.org/packages/source/s/setuptools/setuptools-6.1.tar.gz
-  # 106  tar xvzf setuptools-6.1.tar.gz 
-  # 107  cd setuptools-6.1/
-  # 108  python ez_setup.py
-  # 109  easy_install pip
-
-  # 111  pip install flask
-  # 112  pip install flask-login
-  # 113  pip install flask-openid
-  # 114  pip install flask-mail
-  # 115  pip install flask-sqlalchemy
-  # 116  pip install sqlalchemy-migrate
-  # 117  pip install flask-whooshalchemy
-  # 118  pip install flask-wtf
-  # 119  pip install flask-babel
-  # 120  pip install guess_language
-  # 121  pip install flipflop
-  # 122  pip install coverage
+sudo apt-get install -y curl
+sudo apt-get install -y python3
 
 # restart
 source ~/.bashrc
